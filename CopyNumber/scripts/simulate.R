5eeb5dd7b18fdda1c9e36cc7b165f1ea1e4057a1
Sara Cocomello <sara.cocomello@gmail.com>
2024-07-31 21:32:05 +0200

    fix model selection plot

--- CopyNumber/scripts/simulate.R
+++ CopyNumber/scripts/simulate.R
@@ -82,6 +82,8 @@ for(i in 1:n_simulations){
     purity = purity
   )
   
+  
+  #in "fit model selection best K" the plots for each K inference is directly saved 
   results <- fit_model_selection_best_K(data_sim, data$karyo, purity, INIT = INIT, simulation_params = simulation_params)
   
   
@@ -91,7 +93,84 @@ for(i in 1:n_simulations){
   
   
   model_selection <- results$model_selection_tibble
-  saveRDS(model_selection, "model_selection.rds")
+  best_K <- results$best_K
+  
+  
+  bic_plot <- ggplot(data = model_selection, aes(x = K, y = BIC)) + 
+    geom_line(aes(colour = "BIC Line")) + 
+    geom_point(aes(colour = "BIC Point")) +
+    geom_point(aes(x = best_K, y = BIC[K == best_K], colour = "Best K Point")) +
+    geom_point(aes(x = K[BIC == min(BIC)], y = min(BIC), colour = "Minimum Point")) +
+    scale_colour_manual(name = "Legend",
+                        values = c("BIC Line" = "blue", 
+                                   "BIC Point" = "blue", 
+                                   "Best K Point" = "red",
+                                   "Minimum Point" = "green"),)
+  
+  aic_plot <- ggplot(data = model_selection, aes(x = K, y = AIC)) + 
+    geom_line(aes(colour = "AIC Line")) + 
+    geom_point(aes(colour = "AIC Point")) +
+    geom_point(aes(x = best_K, y = AIC[K == best_K], colour = "Best K Point")) +
+    geom_point(aes(x = K[AIC == min(AIC)], y = min(AIC), colour = "Minimum Point")) +
+    scale_colour_manual(name = "Legend",
+                        values = c("AIC Line" = "blue", 
+                                   "AIC Point" = "blue", 
+                                   "Best K Point" = "red",
+                                   "Minimum Point" = "green"))
+  
+
+  loo_plot <- ggplot(data = model_selection, aes(x = K, y = LOO)) + 
+    geom_line(aes(colour = "LOO Line")) + 
+    geom_point(aes(colour = "LOO Point")) +
+    geom_point(aes(x = best_K, y = LOO[K == best_K], colour = "Best K Point")) +
+    geom_point(aes(x = K[LOO == min(LOO)], y = min(LOO), colour = "Minimum Point")) +
+    scale_colour_manual(name = "Legend",
+                        values = c("LOO Line" = "blue", 
+                                   "LOO Point" = "blue", 
+                                   "Best K Point" = "red",
+                                   "Minimum Point" = "green"))
+  
+  
+  log_lik_plot <- ggplot(data = model_selection, aes(x = K, y = Log_lik)) + 
+    geom_line(aes(colour = "Log likelihood Line")) + 
+    geom_point(aes(colour = "Log likelihood Point")) +
+    geom_point(aes(x = best_K, y = Log_lik[K == best_K], colour = "Best K Point")) +
+    geom_point(aes(x = K[Log_lik == min(Log_lik)], y = min(Log_lik), colour = "Minimum Point")) +
+    scale_colour_manual(name = "Legend",
+                        values = c("Log likelihood Line" = "blue", 
+                                   "Log likelihood Point" = "blue", 
+                                   "Best K Point" = "red",
+                                   "Minimum Point" = "green"))
+  
+  
+  
+  Subtitle <- vector("list", S)
+  
+  for (i in 1:S) {
+    n_parameters <- i+(i*S)+2
+    Subtitle[[i]] <- paste0(" ", i, ": ", n_parameters," parameters")
+  }
+  
+  Subtitle <- paste(Subtitle, collapse = "\n")
+  
+  
+  
+  
+  model_selection_plot <- (bic_plot | aic_plot) / (loo_plot|log_lik_plot) +
+  plot_annotation(
+    title = "Model selection graphs: score vs number of clusters" ,
+    subtitle = paste0 ("Correspondence between number of clusters and number of parameters:  \n", Subtitle),
+    caption = "caption"
+  ) & theme(text = element_text(size = 8), plot.title = element_text(size = 10), plot.subtitle = element_text(size = 8), axis.text = element_text(size = 8), plot.caption = element_text(size = 5))
+  
+  
+  
+  
+  
+  saveRDS(model_selection_plot, "model_selection.rds")
+  
+  
+  
   setwd(original_dir)
   #plot togeter with loo
   #barplot(table(d$x))
